3913dd9e0a828ffca17edbf5f31d4f922278f7d0
Amin Razzak <amin.hybrid77@gmail.com>
2013-11-25 14:43:41 +0100

    Ik heb mappen gemaakt voor alle game scenes en eentje voor game.

---
+++ Pyramidpanic/Pyramidpanic/Pyramidpanic/GameScenes/StartScene/StartScene.cs
@@ -0,0 +1,60 @@
+﻿//met using kan je een microsoft xna codebibliotheek toevoegen gebruiken in je class.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PyramidPanic
+{
+    public class StartScene
+    {
+        //de constructor ( heeft de zelfde naam als de class)
+    public StartScene(){
+    
+    
+    
+    }
+
+
+    //Initialize method. Deze methode instaleert ( geeft de start waarden aan de variabelen)
+    //void wilt zeggen dat er niets teruggegeven word.
+
+    public void Initialize()
+    {
+    
+    
+    }
+
+        //LoadConent methode. Deze methode maakt nieuwe objecten aan van de verschilende
+        //classes
+    public void LoadConent() { 
+    
+    
+    
+    }
+
+        // Update methode. Deze methode wordt normaal 60 maal per seconde aangeroepen.
+        //en update alle variabelen, methods enz....
+    public void Update() { 
+    
+    
+    
+    }
+    //Draw methode. Deze methode word normaal 60 maal perseconde aangeroepen en
+        //tekent de textures op het canvas
+    public void Draw() { 
+    
+    
+    
+    
+    }
+
+
+    }
+}
