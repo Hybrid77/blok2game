574539fe3792f0a20b602f9dbf2284b8656eb598
Amin Razzak <amin.hybrid77@gmail.com>
2014-01-30 17:53:55 +0100

    ik heb het commentaar bij scorpion gezet wat daar nodig was om sneller door de code heen te gaan

--- Pyramidpanic/Pyramidpanic/Pyramidpanic/GameScenes/PlayScene/Scorpion/WalkRight.cs
+++ Pyramidpanic/Pyramidpanic/Pyramidpanic/GameScenes/PlayScene/Scorpion/WalkRight.cs
@@ -19,15 +19,24 @@ namespace PyramidPanic
     public class WalkRight : AnimatedSprite, IEntityState
     {
         //fields
+        //hier kijgt de scorpion class de naam scorpion
         private Scorpion scorpion;
+        //hier word de vector2 als velocity aangegeven.
         private Vector2 velocity;
 
         //constructor
         public WalkRight(Scorpion scorpion) : base(scorpion)
         {
             this.scorpion = scorpion;
-            this.destinationRectangle = new Rectangle((int)this.scorpion.Position.X, (int)this.scorpion.Position.Y, 32, 32);
-            this.velocity = new Vector2(this.scorpion.Speed, 0f);
+            //hier word aangegeven dat de scorpion niet word omgedraait
+            this.effect = SpriteEffects.None;
+            //dit geeft de positie van de scorpion aan en de breedte en lengte 
+            this.destinationRectangle = new Rectangle((int)this.scorpion.Position.X,
+                                                      (int)this.scorpion.Position.Y,
+                                                       32,
+                                                       32);
+            //hier word de loop snelheid aangegeven van de beetle
+            this.velocity = new Vector2(0f, this.scorpion.Speed);
         }
 
 
@@ -42,14 +51,22 @@ namespace PyramidPanic
         //update method
         public new void Update(GameTime gameTime) 
         {
-
+            //als de posietie van de scorpion groter is dan 640 - 17px dan:
             if (this.scorpion.Position.X > 640-17) 
             {
+                //word de state van de scorpionx verandert naar walkleft
                 this.scorpion.State = new WalkLeft(this.scorpion);
+                //hier word de initialize method aangeroepen van de walkleft class
                 this.scorpion.WalkLeft.Initialize();
             }
+
+            //dit zorgt er voor dat de scorpion naar rechts beweegt
             this.scorpion.Position += this.velocity;
+            //dit zegt dat de destination rectangle x
+            //gelijk staat aan de positie van de scorpion op de x as.
             this.destinationRectangle.X = (int)this.scorpion.Position.X;
+            //dit zegt dat de destination rectangle y
+            //gelijk staat aan de positie van de scorpion op de y as.
             this.destinationRectangle.Y = (int)this.scorpion.Position.Y;
             base.Update(gameTime);
         }
